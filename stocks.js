4fb10676a1f527155127d302d08404d0fdb2f0b2
tokenasian <78766033+tokenasian@users.noreply.github.com>
2022-04-25 18:12:10 -0400

    Create stocks.js

---
+++ stocks.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const stocks = [
+    { company: 'Splunk', symbol: 'SPLK', price: 137.55 },
+    { company: 'Microsoft', symbol: 'MSFT', price: 232.04 },
+    { company: 'Oracle', symbol: 'ORCL', price: 67.08 },
+    { company: 'Snowflake', symbol: 'SNOW', price: 235.8 },
+    { company: 'Teradata', symbol: 'TDC', price: 44.98 }
+];
+
+module.exports.stocks = stocks;
